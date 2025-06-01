7a9118ad05cad459f11774c0df28a9502b984113
heojin-dev <gjwls031456@gmail.com>
2025-06-01 14:59:14 +0900

    Add chapter 1 example code

---
+++ src/ch01/sec11/Calculator.java
@@ -0,0 +1,13 @@
+package ch01.sec11;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int x = 1; 					//변수 x를 선언하고 1 저장
+		int y = 2;					//변수 y를 선언하고 2 저장
+		int result = x + y;			//변수 result를 선언하고 x와 y 더한값 저장
+		System.out.println(result); //콘솔에 출력하는 메소드 호출
+
+	}
+
+}
